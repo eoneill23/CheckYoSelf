c852e054e3b22f1dd31f962179f1bbdaef02af88
Eric O'Neill <oneill.eric23@gmail.com>
2019-05-31 19:05:21 -0600

    Finalize enable/disable form buttons in JavaScript

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-document.querySelector('.form__inputTitle');
-document.querySelector('.div__inputAddTask');
+var taskTitleInput = document.querySelector('.form__inputTitle');
+var taskListInput = document.querySelector('.div__inputAddTask');
+var makeTaskBtn = document.querySelector('.form__btnsAddTask');
+var clearBtn = document.querySelector('.form__btnsClear');
+var addBtn = document.querySelector('.div__btnAddTask')
+
+taskTitleInput.addEventListener('keyup', enableMCABtns);
+taskListInput.addEventListener('keyup', enableMCABtns);
+makeTaskBtn.addEventListener('click',)
+
+function enableMCABtns() {
+  makeTaskBtn.disabled = false;
+  clearBtn.disabled = false;
+  addBtn.disabled = false;
+  disableMCABtns();
+}
+
+function disableMCABtns() {
+  if (taskTitleInput.value === '' && taskListInput.value === '') {
+  makeTaskBtn.disabled = true;
+  clearBtn.disabled = true;
+  addBtn.disabled = true;
+  }
+}
+
+function createTaskItem() {
+
+}
