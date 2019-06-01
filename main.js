dc1f43875442e795c8ffa5ae2614596b43f1a2e3
Eric O'Neill <oneill.eric23@gmail.com>
2019-05-31 19:22:16 -0600

    Get task list items to append on DOM

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+var taskItems = [];
 var taskTitleInput = document.querySelector('.form__inputTitle');
 var taskListInput = document.querySelector('.div__inputAddTask');
 var makeTaskBtn = document.querySelector('.form__btnsAddTask');
 var clearBtn = document.querySelector('.form__btnsClear');
-var addBtn = document.querySelector('.div__btnAddTask')
+var addBtn = document.querySelector('.div__btnAddTask');
+var taskListItems = document.querySelector('.form__ul');
+var taskListUl = document.querySelector('.form__ul')
 
 taskTitleInput.addEventListener('keyup', enableMCABtns);
 taskListInput.addEventListener('keyup', enableMCABtns);
-makeTaskBtn.addEventListener('click',)
+addBtn.addEventListener('click', createTaskItem)
 
 function enableMCABtns() {
   makeTaskBtn.disabled = false;
@@ -23,6 +26,12 @@ function disableMCABtns() {
   }
 }
 
-function createTaskItem() {
-
+function createTaskItem(event) {
+  event.preventDefault();
+  var taskId = Date.now();
+  var taskItem = `
+    <li>
+    <img src="images/delete.svg" class="form__liImg" id="${taskId}">${taskListInput.value}
+    </li>`
+  taskListUl.innerHTML += taskItem;
 }
